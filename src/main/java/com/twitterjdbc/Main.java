a048b7f169e7faebdad4c10a5df6fda01bf223fe
jose <joseanperezdp@gmail.com>
2025-01-08 20:48:58 +0100

    CREADOS MÉTODOS DE VER PERFIL, CREAR PUBLICACION Y VER PUBLICACIONES DEL USUARIO - Se han creados los métodos mostrarTuPerfil, publicar, ejecutarPublicar y mostrarTusPublicaciones

--- src/main/java/com/twitterjdbc/Main.java
+++ src/main/java/com/twitterjdbc/Main.java
@@ -76,6 +76,110 @@ public class Main {
         sentencia.close();
     }
 
+    /*
+        Crear un método showYourProfile que reciba una conexión y muestre por pantalla el nombre de
+        usuario, email, descripción y fecha de registro del userID estático.
+     */
+
+    public static void mostrarTuPerfil(Connection con) throws SQLException {
+        String query = "SELECT username, email, description, createDate FROM users WHERE id = " + usuarioID + ";";
+        Statement sentencia = con.createStatement();
+        ResultSet resultado = sentencia.executeQuery(query);
+        resultado.next();
+        System.out.println(resultado.getString(1) + " | Creada el " + resultado.getString(4));
+        System.out.println("\t[" + resultado.getString(2) + "]");
+        System.out.println("\t" + resultado.getString(3));
+        resultado.close();
+        sentencia.close();
+    }
+
+    /*
+        Crear un método tweetear que reciba una conexión y un String con el texto a twittear y que inserte una
+        publicación con el userID estático, el texto pasado por pantalla y la fecha actual
+     */
+    public static void ejecutarPublicar(Connection con) throws SQLException {
+        System.out.print("¿Qué quieres publicar?:\n\t> ");
+        String texto = SCANNER.nextLine();
+        publicar(con, texto);
+    }
+
+    public static void publicar(Connection con, String texto) throws SQLException {
+        String query = "INSERT INTO publications (userId, text, createDate) VALUES(?, ?, ?);";
+        PreparedStatement sentencia = con.prepareStatement(query);
+        LocalDateTime registerDate = LocalDateTime.now();
+        DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd-HH:mm:ss");
+        String formatedDate = registerDate.format(dateFormat);
+        sentencia.setInt(1, usuarioID);
+        sentencia.setString(2, texto);
+        sentencia.setString(3, formatedDate);
+        sentencia.executeUpdate();
+        sentencia.close();
+        System.out.println("Publicado correctamente");
+    }
+
+    /*
+        Crear un método showYourTweets que reciba una conexión y muestre por pantalla el id de la publicación,
+        el nombre de usuario de userID, el texto y la fecha en la que se creo la publicación.
+     */
+    public static void mostrarTusPublicaciones(Connection con) throws SQLException {
+        String query = "SELECT publications.id, users.username, publications.text, publications.createDate " +
+                       "FROM publications JOIN users ON users.id = publications.userId " +
+                       "WHERE publications.userId = " + usuarioID + ";";
+        Statement sentencia = con.createStatement();
+        ResultSet resultado = sentencia.executeQuery(query);
+        while(resultado.next()) {
+            System.out.println("[" + resultado.getInt(1) + "] - " + resultado.getString(2) +
+                               "\n\t" + resultado.getString(3) + "\n\t-Creada el " +
+                               resultado.getString(4) + "-");
+        }
+        resultado.close();
+        sentencia.close();
+    }
+
+
+    /*
+        Crear un método showFollowedTweets que reciba una conexión y muestre por pantalla el nombre del usuario,
+        el texto del tweet y la fecha en la que se publicó el tweet de los tweets que hayan publicado las personas
+        que sigues. Deben aparecer en orden de más nuevo a más viejo.
+     */
+    /*
+        Crear un método showTweets que reciba una conexión y muestre por pantalla el nombre del usuario, el texto
+        del tweet y la fecha en la que se publicó el tweet de todos los tweets. Deben aparecer en orden de más
+        nuevo a más viejo.
+     */
+
+    /*
+        Crear un método showAllProfiles que reciba una conexión y muestra por pantalla id, nombre de usuario,
+        email, descripción y fecha de registro de todos los usuarios menos del usuario cuyo id sea idéntico
+        al userID estático.
+     */
+
+    /*
+        Crear un método deleteTweet que reciba una conexión y un id y que elimine la publicación cuyo id de
+        publicación coincida con el id pasado como argumento Y QUE EL ID DEL USUARIO QUE LA PUBLICÓ COINCIDA
+        CON EL ATRIBUTO ESTÁTICO userID.
+     */
+    /*
+        Crear un método showYourFollows que reciba una conexión y muestre por pantalla todos los datos de
+        los usuarios que sigues.
+     */
+    /*
+        Crear un método showYourFollowers que reciba una conexión y muestre por pantalla todos los datos de los
+        usuarios que siguen al atributo userID estático.
+     */
+    /*
+        Crear un método estático showOtherProfile que dada una conexión y un nombre de usuario te muestre todos
+        los datos de ese usuario.
+     */
+    /*
+        Crear un método follow que reciba una conexión y un id al que seguir e insertar en la tabla follows que
+        el userID sigue al id pasado como argumento.
+     */
+    /*
+        Crear un método unfollow que reciba una conexión y un id al que seguir y borrar en la tabla follows el
+        registro que representa que el userID sigue al id pasado como argumento.
+     */
+
     public static void elegirRegistrarOiniciarSesion(Connection con) throws Exception {
         boolean bandera = true;
         while (bandera) {
@@ -98,6 +202,48 @@ public class Main {
                     break;
                 case 2:
                     ejecutarIniciarSesion(con);
+                    System.out.println();
+                    Thread.sleep(1000);
+                    elegirAccionesDeUsuario(con);
+                    Thread.sleep(1000);
+                    System.out.println();
+                    break;
+                default:
+                    System.out.println("El índice introducido no es correcto, inténtalo de nuevo.");
+                    break;
+            }
+        }
+    }
+
+    public static void elegirAccionesDeUsuario(Connection con) throws Exception {
+        boolean bandera = true;
+        while (bandera) {
+            System.out.println("Elige una de estas opciones: ");
+            System.out.println("\t[1] Perfil");
+            System.out.println("\t[2] Publicar");
+            System.out.println("\t[3] Ver tus publicaciones");
+            System.out.println("\t[0] Cerrar sesión");
+            System.out.print("Acción: ");
+            int accion = Integer.parseInt(SCANNER.nextLine());
+            System.out.println();
+            switch (accion) {
+                case 0:
+                    System.out.println("Sesión cerrada con éxito");
+                    bandera = false;
+                    usuarioID = -1;
+                    break;
+                case 1:
+                    mostrarTuPerfil(con);
+                    System.out.println();
+                    Thread.sleep(1000);
+                    break;
+                case 2:
+                    ejecutarPublicar(con);
+                    System.out.println();
+                    Thread.sleep(1000);
+                    break;
+                case 3:
+                    mostrarTusPublicaciones(con);
                     System.out.println();
                     Thread.sleep(1000);
                     break;
